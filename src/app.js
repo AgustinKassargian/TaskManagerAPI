8d18d06388f474f2b4e2aa7a797490c8794d938e
Agustin Kassargian <agustinkassargian@gmail.com>
2024-12-31 19:18:50 -0300

    cors

--- src/app.js
+++ src/app.js
@@ -1,9 +1,10 @@
 import express from "express";
 import morgan from "morgan";
 import router from "./routes/router.js";
+import cors from "cors";
 
 const app = express();
-
+app.use(cors())
 app.use(morgan('dev'));
 app.use(express.json());
 app.use('/api',router);
