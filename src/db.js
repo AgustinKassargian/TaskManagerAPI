733b68df88c120de7d8b416d0242bf6901d42d53
Agustin Kassargian <agustinkassargian@gmail.com>
2024-12-31 14:48:33 -0300

    env example

--- src/db.js
+++ src/db.js
@@ -3,15 +3,12 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const DB_USER = process.env.DB_USER
-const DB_PASSWORD = process.env.DB_PASSWORD
-const CLUSTER = process.env.CLUSTER
-const DB_NAME = process.env.DB_NAME;
+const DB_URL = process.env.DB_URL
 
 export const connectDB = async()=>{
     try {
-        console.log(`Conectando a la base de datos en ${DB_NAME} como ${DB_USER}`);
-        await mongoose.connect(`mongodb+srv://${DB_USER}:${DB_PASSWORD}@${CLUSTER}.7kjqm.mongodb.net/${DB_NAME}?retryWrites=true&w=majority&appName=Cluster0`)
+        console.log(`Conectando a la base de datos.`);
+        await mongoose.connect(DB_URL)
         console.log('DB connected')
         
     } catch (error) {
